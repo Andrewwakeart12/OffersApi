90e3f1139fa39da1a346cc60a9eb5e3406e1de8a
Andrewwakeart12 <andrewwake.art@gmail.com>
2022-04-11 00:37:43 -0400

    bucle works just fine

--- test/testingDinamycInputs.js
+++ test/testingDinamycInputs.js
@@ -12,4 +12,21 @@ try {
 }
 
 }
-regMod();
+async function regModBucle(){
+    try {
+        var names = ['amazon','liverpool'];
+        for(let name of names){
+            var protocolName = 'Scraper';
+            var imp =  `./../scrapper/${name}/` + name + protocolName + '.js';
+            var GeneralData = await import(imp)
+            var {DERR, Scraper} = GeneralData
+            var deer = new DERR(`${name} says hi to you`);
+            console.log(deer.message);
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+    }
+    regModBucle();

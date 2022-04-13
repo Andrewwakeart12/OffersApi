74e7f56a7733f78bf9bb2f0d38c7972607916ed5
Andrewwakeart12 <andrewwake.art@gmail.com>
2022-04-13 13:37:49 -0400

    e

--- scrapper/browser.js
+++ scrapper/browser.js
@@ -87,7 +87,7 @@ async function startBrowser(){
        console.log("Opening the browser......");
         browser = await puppeteer.launch({
             pipe: true,
-            headless: false,
+            headless: true,
             ignoreHTTPSErrors: true,
             slowMo: 0,
             userAgent: randomUA.generate(),

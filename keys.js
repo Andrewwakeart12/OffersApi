3d3c6bb105f68502c395f49a4cf4968797deaf38
Andrewwakeart12 <andrewwake.art@gmail.com>
2022-05-21 15:13:35 -0400

    timeout added to db

--- keys.js
+++ keys.js
@@ -1,9 +1,11 @@
 import {} from 'dotenv/config';
 
 export const database = {
-    connectionLimit: 10,
+    connectionLimit: 200,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout: 300000
+
 };
